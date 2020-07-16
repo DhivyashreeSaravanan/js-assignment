f84f532465b653dc35280591228ccb832c701e9d
DhivyashreeSaravanan <68265712+DhivyashreeSaravanan@users.noreply.github.com>
2020-07-16 10:17:29 +0530

    js-Assignment_day2

---
+++ index.js
@@ -0,0 +1,7 @@
+let a=prompt("cube of 8?");
+console.log(a);
+let c=8**3;
+if(a==c)
+console.log(" well done!")
+else
+console.log("it's 512")
---
+++ index1.js
@@ -0,0 +1,5 @@
+let b=prompt("enter your age");
+if(b>=21)
+console.log("can drink");
+else
+console.log("can't drink");
